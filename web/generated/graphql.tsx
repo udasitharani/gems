8d2bf96a11d53dc1a6811d3de45b896d2945ef0e
Udasi Tharani <udasitharani@gmail.com>
2021-08-08 21:35:40 +0530

    implemented signup

--- web/generated/graphql.tsx
+++ web/generated/graphql.tsx
@@ -59,6 +59,16 @@ export type UserResponse = {
   user?: Maybe<User>;
 };
 
+export type CreateMutationVariables = Exact<{
+  createPassword: Scalars['String'];
+  createEmail: Scalars['String'];
+  createName: Scalars['String'];
+  createUsername: Scalars['String'];
+}>;
+
+
+export type CreateMutation = { __typename?: 'Mutation', create: { __typename?: 'UserResponse', error?: Maybe<string>, user?: Maybe<{ __typename?: 'User', email: string, id: number, name: string, username: string }> } };
+
 export type LoginMutationVariables = Exact<{
   loginPassword: Scalars['String'];
   loginUsernameOrEmail: Scalars['String'];
@@ -73,6 +83,53 @@ export type MeQueryVariables = Exact<{ [key: string]: never; }>;
 export type MeQuery = { __typename?: 'Query', me: { __typename?: 'UserResponse', error?: Maybe<string>, user?: Maybe<{ __typename?: 'User', email: string, id: number, name: string, username: string }> } };
 
 
+export const CreateDocument = gql`
+    mutation create($createPassword: String!, $createEmail: String!, $createName: String!, $createUsername: String!) {
+  create(
+    password: $createPassword
+    email: $createEmail
+    name: $createName
+    username: $createUsername
+  ) {
+    error
+    user {
+      email
+      id
+      name
+      username
+    }
+  }
+}
+    `;
+export type CreateMutationFn = Apollo.MutationFunction<CreateMutation, CreateMutationVariables>;
+
+/**
+ * __useCreateMutation__
+ *
+ * To run a mutation, you first call `useCreateMutation` within a React component and pass it any options that fit your needs.
+ * When your component renders, `useCreateMutation` returns a tuple that includes:
+ * - A mutate function that you can call at any time to execute the mutation
+ * - An object with fields that represent the current status of the mutation's execution
+ *
+ * @param baseOptions options that will be passed into the mutation, supported options are listed on: https://www.apollographql.com/docs/react/api/react-hooks/#options-2;
+ *
+ * @example
+ * const [createMutation, { data, loading, error }] = useCreateMutation({
+ *   variables: {
+ *      createPassword: // value for 'createPassword'
+ *      createEmail: // value for 'createEmail'
+ *      createName: // value for 'createName'
+ *      createUsername: // value for 'createUsername'
+ *   },
+ * });
+ */
+export function useCreateMutation(baseOptions?: Apollo.MutationHookOptions<CreateMutation, CreateMutationVariables>) {
+        const options = {...defaultOptions, ...baseOptions}
+        return Apollo.useMutation<CreateMutation, CreateMutationVariables>(CreateDocument, options);
+      }
+export type CreateMutationHookResult = ReturnType<typeof useCreateMutation>;
+export type CreateMutationResult = Apollo.MutationResult<CreateMutation>;
+export type CreateMutationOptions = Apollo.BaseMutationOptions<CreateMutation, CreateMutationVariables>;
 export const LoginDocument = gql`
     mutation login($loginPassword: String!, $loginUsernameOrEmail: String!) {
   login(password: $loginPassword, usernameOrEmail: $loginUsernameOrEmail) {
---
+++ web/pages/signup.tsx
@@ -0,0 +1,111 @@
+import React, { useState } from "react";
+import AuthenticationLayout from "../components/authenticationLayout";
+import InputField from "../components/inputField";
+import StateButton from "../components/stateButton";
+import { useCreateMutation } from "../generated/graphql";
+import {
+  validateEmail,
+  validateName,
+  validatePassword,
+  validateUsername,
+} from "../utils/validation";
+
+const Login: React.FC = () => {
+  const [email, setEmail] = useState("");
+  const [username, setUsername] = useState("");
+  const [name, setName] = useState("");
+  const [password, setPassword] = useState("");
+  const [showEmailError, setShowEmailError] = useState<boolean>(false);
+  const [showUsernameError, setShowUsernameError] = useState<boolean>(false);
+  const [showNameError, setShowNameError] = useState<boolean>(false);
+  const [showPasswordError, setShowPasswordError] = useState<boolean>(false);
+  const [create, { loading, data }] = useCreateMutation();
+
+  return (
+    <AuthenticationLayout>
+      <section className="flex-1 px-8 sm:px-16 md:px-40 py-16 md:py-40">
+        <header className="mb-8 text-center text-2xl text-darkBlue font-semibold opacity-0 animate-jumpUp">
+          Create Your Gems Account
+        </header>
+        <div className="w-full sm:w-3/4 md:w-2/3 mx-auto">
+          <p className="mb-2 text-sm text-red-500">{data?.create.error}</p>
+          <InputField
+            classNames="opacity-0 animate-jumpUp"
+            label="Name"
+            value={name}
+            setValue={setName}
+            showError={showNameError}
+            errorMsg={`Are you kidding me? ${name}?`}
+            validationFn={() => {
+              if (!validateName(name) && name) setShowNameError(true);
+              else setShowNameError(false);
+            }}
+          />
+          <InputField
+            classNames="opacity-0 animate-jumpUp"
+            label="Username"
+            value={username}
+            setValue={setUsername}
+            showError={showUsernameError}
+            errorMsg={`A username can contain only alphabets, numbers, '_' and '.'`}
+            validationFn={() => {
+              if (!validateUsername(username) && username)
+                setShowUsernameError(true);
+              else setShowUsernameError(false);
+            }}
+          />
+          <InputField
+            classNames="opacity-0 animate-jumpUp"
+            label="Email"
+            value={email}
+            setValue={setEmail}
+            showError={showEmailError}
+            errorMsg="Invalid email."
+            validationFn={() => {
+              if (!validateEmail(email) && email) setShowEmailError(true);
+              else setShowEmailError(false);
+            }}
+          />
+          <InputField
+            classNames="opacity-0 animate-jumpUp"
+            label="Password"
+            value={password}
+            type="password"
+            setValue={setPassword}
+            errorMsg="Password is too weak. It should have at least one uppercase letter, one lowercase letter, one digit, one special character, and should be at least 8 characters long."
+            showError={showPasswordError}
+            validationFn={() => {
+              if (!validatePassword(password) && password)
+                setShowPasswordError(true);
+              else setShowPasswordError(false);
+            }}
+          />
+          <StateButton
+            wrapperClassNames="opacity-0 animate-jumpUp"
+            classNames="block ml-auto"
+            label="Create"
+            state={
+              loading
+                ? "fetching"
+                : !showEmailError && email && password
+                ? "enabled"
+                : "disabled"
+            }
+            onClick={async () => {
+              await create({
+                variables: {
+                  createEmail: email,
+                  createName: name,
+                  createPassword: password,
+                  createUsername: username,
+                },
+              });
+            }}
+          />
+        </div>
+      </section>
+    </AuthenticationLayout>
+  );
+};
+
+export default Login;
