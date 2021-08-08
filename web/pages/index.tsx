458b7cfcbcd7c112e690d3733df61dbfe1844b4c
Udasi Tharani <udasitharani@gmail.com>
2021-08-08 21:50:43 +0530

    minimal home page

--- web/pages/index.tsx
+++ web/pages/index.tsx
@@ -1,3 +1,22 @@
+import { useMeQuery } from "../generated/graphql";
+import Link from "next/link";
+
 export default function Home() {
-  return <div></div>;
+  const { data } = useMeQuery();
+  return (
+    <main className="mx-auto w-max">
+      <div>Hello, {data?.me.user?.name}</div>
+      <br />
+      <div>
+        <Link href="/login">
+          <a>Login</a>
+        </Link>
+      </div>
+      <div>
+        <Link href="/signup">
+          <a>Create Account</a>
+        </Link>
+      </div>
+    </main>
+  );
 }
